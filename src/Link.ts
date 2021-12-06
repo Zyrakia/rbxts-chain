e68367500f3c9193570b351dce42a854d90cd1ed
Zyrakia <mailzyrakia@gmail.com>
2021-12-05 21:05:46 -0800

    fix: removed old generic type

--- src/Link.ts
+++ src/Link.ts
@@ -2,7 +2,7 @@ import { ChainSession } from 'ChainSession';
 
 export interface Link<T> {
 	/** Called when the link is asked to participate in the specified session. */
-	handle: (this: Link<T>, data: T, session: ChainSession<T>) => void | Promise<void>;
+	handle: (this: Link<T>, data: T, session: ChainSession) => void | Promise<void>;
 
 	/** Called when a session that the link participated in was cancelled. */
 	onCancel?: (this: Link<T>) => void | Promise<void>;
@@ -10,7 +10,7 @@ export interface Link<T> {
 
 export interface FLink<T> {
 	/** Called when the link is asked to participate in the specified session. */
-	(data: T, session: ChainSession<T>): void | Promise<void>;
+	(data: T, session: ChainSession): void | Promise<void>;
 }
 
 export type ChainLink<T> = Link<T> | FLink<T>;
